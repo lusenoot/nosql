8026e43b0c1e844d10250a5af29a7730e43af4a5
lusen <lusen@lusen.com>
2017-05-22 15:09:30 +0800

    client cmdline add subcommand to operate db

--- src/client.py
+++ src/client.py
@@ -11,11 +11,27 @@ def __parse_args():
     parser.add_argument("--ip", help="Server listening IP address [default: 127.0.01]", default="127.0.0.1")
     parser.add_argument("--port", help="Server listening port [default: 9527]", type=int, default=9527)
 
-    parser.add_argument("--cmd", action="store", default="get",
-                        choices=["put", "plist", "get", "delete", "select", "keys"], help="Operation")
-    parser.add_argument("--key", action="store", help="Key")
-    parser.add_argument("--value", action="store", help="Value")
-    parser.add_argument("--dbid", action="store", default=0, help="dbid")
+    subparsers = parser.add_subparsers(dest="command", metavar="command")
+    subparsers.required = True
+
+    put_parser = subparsers.add_parser("put", help="put Key-Value to db [dbid = 0]")
+    put_parser.add_argument("kvargs", nargs="+", metavar="kvargs", help="kv params splited by \";\"")
+
+    plist_parser = subparsers.add_parser("plist",
+            help="put Key-Value(Value splited by \",\") to db [dbid = 0]")
+    plist_parser.add_argument("kvargs", nargs="+", metavar="kvargs", help="kv params splited by \";\"")
+
+    get_parser = subparsers.add_parser("get", help="get Value from db by Key [dbid = 0]")
+    get_parser.add_argument("kvargs", nargs="+", metavar="kvargs", help="key params splited by \";\"")
+
+    delete_parser = subparsers.add_parser("delete", help="delete Key-Value from db by Key [dbid = 0]")
+    delete_parser.add_argument("kvargs", nargs="+", metavar="kvargs", help="key params splited by \";\"")
+
+    select_parser = subparsers.add_parser("select", help="switch db by dbid")
+    select_parser.add_argument("kvargs", nargs="?", metavar="kvargs", help="user dbid")
+
+    keys_parser = subparsers.add_parser("keys", help="list all keys by dbid")
+    keys_parser.add_argument("kvargs", nargs="?", metavar="kvargs", help="user dbid")
 
     return parser.parse_args()
 
@@ -34,25 +50,47 @@ def is_number(value):
 
     return False
 
+def __parse_dbid(kvargs, limited):
+    dbid = 0
+    try:
+        if len(kvargs) >= limited:
+            dbid = int(kvargs[limited - 1])
+    except:
+        dbid = 0
+
+    return dbid
+
+
 def main():
     args = __parse_args()
 
-    valuetype = "string"
-    if args.value is not None:
-        if is_number(args.value):
-            valuetype = "number"
-        elif "," in args.value:
+    if args.command in ("put", "plist"):
+        if len(args.kvargs) < 2:
+            print("params error, need Key Value at least")
+            sys.exit(1)
+
+        valuetype = "string"
+        if args.kvargs[1] is not None:
+            if is_number(args.kvargs[1]):
+                valuetype = "number"
+            elif "," in args.kvargs[1]:
+                valuetype = "list"
+
+        if args.command == "plist":
             valuetype = "list"
 
-    if args.cmd == "plist":
-        valuetype = "list"
+        dbid = __parse_dbid(args.kvargs, 3)
+        data = u"{0}; {1}; {2}; {3}; {4}".format(args.command, args.kvargs[0], args.kvargs[1], valuetype, dbid)
+    elif args.command in ("get", "delete"):
+        if len(args.kvargs) < 1:
+            print("params error, need Key at least")
+            sys.exit(1)
 
-    if args.key and args.value and args.cmd in ("put", "plist"):
-        data = u"{0}; {1}; {2}; {3}; {4}".format(args.cmd, args.key, args.value, valuetype, args.dbid)
-    elif args.key and args.cmd in ("get", "delete"):
-        data = u"{0}; {1}; {2}".format(args.cmd, args.key, args.dbid)
-    elif args.cmd in ("select", "keys"):
-        data = u"{0}; {1}".format(args.cmd, args.dbid)
+        dbid = __parse_dbid(args.kvargs, 2)
+        data = u"{0}; {1}; {2}".format(args.command, args.kvargs[0], dbid)
+    elif args.command in ("select", "keys"):
+        dbid = __parse_dbid(args.kvargs, 1)
+        data = u"{0}; {1}".format(args.command, dbid)
     else:
         sys.exit(1)
 
